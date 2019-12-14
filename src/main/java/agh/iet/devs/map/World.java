a4a63d97a1edd4521de813bde664660f5a7972d9
TemporaryResident <Patrikos112233@interia.pl>
2019-12-14 02:41:10 +0100

    cleaned up world.java

--- src/main/java/agh/iet/devs/map/World.java
+++ src/main/java/agh/iet/devs/map/World.java
@@ -1,6 +1,7 @@
 package agh.iet.devs.map;
 
 import agh.iet.devs.config.Config;
+import agh.iet.devs.data.Vector;
 import agh.iet.devs.elements.AbstractMapElement;
 import agh.iet.devs.elements.MapElement;
 import agh.iet.devs.elements.animal.Animal;
@@ -10,24 +11,20 @@ import agh.iet.devs.map.region.Jungle;
 import agh.iet.devs.map.region.Region;
 import agh.iet.devs.utils.GeneralUtils;
 import agh.iet.devs.view.controller.UpdateListener;
-import agh.iet.devs.data.Vector;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
  * Class responsible for holding information about animals.
- *
- * To avoid using instanceof we could implement Visitor pattern!!!
  */
 public class World implements MapElementObserver, MapElementVisitor {
     private final Config config = Config.getInstance();
     private final UpdateListener listener;
     private final List<Region> regions;
 
-    private final Map<Vector, Set<Animal>> animalDictionary;
-    private final Map<Vector, Food> foodDictionary;
+    private final Map<Vector, Set<Animal>> animals = new HashMap<>();
+    private final Map<Vector, Food> food = new HashMap<>();
 
     public World(UpdateListener listener) {
         this.listener = listener;
@@ -72,81 +69,17 @@ public class World implements MapElementObserver, MapElementVisitor {
     }
 
     public void onUpdate() {
-        regions.stream()
-                .map(Region::emptyPosition)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(freePosition -> new Food(freePosition, config.params.plantEnergy))
-                .forEach(this::attachMapElement);
 
-        regions.stream()
-                .map(Region::objectsInRegion)
-                .flatMap(Collection::stream)
-                .map(Map.Entry::getValue)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList())
-                .forEach(MapElement::onUpdate);
-
-        regions.stream()
-                .map(Region::objectsInRegion)
-                .flatMap(Collection::stream)
-                .map(Map.Entry::getValue)
-                .filter(set -> set.size() >= 2)
-                .forEach(this::handleCollisions);
-
-        final var updated = regions.stream()
-                .map(Region::objectsInRegion)
-                .flatMap(Collection::stream)
-                .map(Map.Entry::getValue)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-
-        listener.onUpdate(updated);
     }
 
-    /**
-     * Note that this method should be called when set contains >= 2 elems.
-     */
-    private void handleCollisions(Set<MapElement> elements) {
-        final var healthiest = findHealthiestAnimal(elements);
-        final var optionalFood = findFood(elements);
-        final var set = new HashSet<>(elements);
-
-        set.remove(healthiest);
-        optionalFood.ifPresent(food -> {
-            healthiest.eat(food);
-            set.remove(food);
-        });
-
-        if (!set.isEmpty()) {
-            final var parent = findHealthiestAnimal(set);
-
-            if (parent.eligibleForReproduction()) {
-                final var kid = Animal.cross(healthiest, parent);
-                attachMapElement(kid);
-            }
-        }
-    }
-
-    private Animal findHealthiestAnimal(Set<MapElement> elements) {
+    private Animal findHealthiestAnimal(Set<Animal> elements) {
         return elements.stream()
-                .filter(e -> e instanceof Animal)
-                .map(e -> (Animal) e)
                 .max(Comparator.comparingInt(AbstractMapElement::getEnergy))
                 .orElseThrow();
-    }
-
-    private Optional<Food> findFood(Set<MapElement> elements) {
-        return elements.stream()
-                .filter(e -> e instanceof Food)
-                .map(f -> (Food) f)
-                .findAny();
     }
 
     private void attachMapElement(MapElement e) {
         regions.forEach(region -> region.attachElement(e));
     }
-
-
 
 }
